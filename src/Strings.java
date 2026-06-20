ccdb5f3a9e0cce9e0fa75b1c767787c5408b336f
Rohit Korpal <rohitkorpal3@gmail.com>
2026-06-20 20:51:12 +0530

    Initial commit

--- src/Strings.java
+++ src/Strings.java
@@ -1,5 +1,17 @@
 public class Strings {
     static void main() {
+        // string is a sequence of characters
+        // character uses ' ' & string uses " "
+        // String str = "Rohit"  <--- declaration (way 1 using string literal)
+        // 2nd is using new keyword ---> string str = new string ("Rohit");
+
+
+
+
+
+
+
+
 
     }
 }
