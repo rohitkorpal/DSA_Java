e51fbd5f39b9f15d0abf909abe456efa2b1cc0dd
Rohit Korpal <rohitkorpal3@gmail.com>
2026-06-21 23:50:01 +0530

    Initial commit

--- src/StringsHW.java
+++ src/StringsHW.java
@@ -1,10 +1,18 @@
 public class StringsHW {
     static void main() {
+        //Q1 count all consonants
 
+//        String str = "rohit";
+//        int count = 0;
+//        for (int i = 0; i < str.length(); i++) {
+//            char ch = str.charAt(i);
+//            if (ch != 'a' && ch != 'e' && ch != 'i' && ch != 'o' && ch != 'u' && ch != 'A' && ch != 'E' && ch != 'I' && ch != 'O' && ch != 'U') {
+//                count++;
+//            }
+//        }
+//        System.out.println(count);
 
-
-
-
+        //Q2 how to remove blank spaces from string
 
 
     }
