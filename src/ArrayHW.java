157d2ff001ecf9181b45a81775ccb37c8017184d
Rohit Korpal <rohitkorpal3@gmail.com>
2026-06-21 21:58:02 +0530

    Initial commit

--- src/ArrayHW.java
+++ src/ArrayHW.java
@@ -38,6 +38,54 @@ public record ArrayHW() {
 //        System.out.println(sum);
 
         //Q4 Search element in matrix
+//        int[][] matrix = {{1, 2, 3},
+//                {4, 5, 6},
+//                {7, 8, 9}
+//        };
+//        int target = 5;
+//        int n = matrix.length;
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < matrix[0].length; j++) {
+//                if (matrix[i][j] == target) {
+//                    System.out.println("Found at row " + i + " column " + j + " = " + target);
+//                }
+//            }
+//        }
+
+        //Q5 to check if a matrix is symmetric
+        // for a matrix to be symmetric--> matrix[i][j] == matrix[j][i]
+//        int[][] matrix = {{1, 2, 3},
+//                {4, 5, 6},
+//                {7, 8, 9}
+//        };
+//        boolean isSymmetric = true;//if we start with false then we have to wait until all comparisons are finished.
+//
+//        for (int i = 0; i < matrix.length; i++) {
+//            for (int j = 0; j < matrix[0].length; j++) {
+//
+//                if (matrix[i][j] != matrix[j][i]) {
+//                    isSymmetric = false;
+//                    break;//only exits inner loop
+//                }
+//            }
+//
+//            //without this code then after finding one mismatch, the program would still continue checking:
+//            if (!isSymmetric) {
+//                break;//exits outer loop
+//            }
+//        }
+//
+//        if (isSymmetric) {
+//            System.out.println("Matrix is Symmetric");
+//        } else {
+//            System.out.println("Matrix is Not Symmetric");
+//        }
+
+        //Q6--> V.IMP - spiral matrix #Leetcode problem 54
+        // spiral?- It keeps going around the outer boundary and then moves inward.
+
+
+
 
 
 
