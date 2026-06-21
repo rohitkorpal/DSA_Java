fe8c0eccbc4dba62da5d4fe192de282ff7891a11
Rohit Korpal <rohitkorpal3@gmail.com>
2026-06-21 16:47:13 +0530

    Initial commit

---
+++ src/ArrayHW.java
@@ -0,0 +1,46 @@
+public record ArrayHW() {
+    static void main() {
+
+        //Q1 print diagonal element
+//        int [][] matrix = { {1,2,3},
+//                {4,5,6},
+//                {7,8,9}
+//        };
+//        int n= matrix.length;
+//        //System.out.println("matrix.length = " + n);
+//        for (int i=0; i<n; i++) {
+//            System.out.println(matrix[i][i]+" ");
+//        }
+
+        //Q2 print anti-diagonal
+//        int[][] matrix = {
+//                {1, 2, 3},
+//                {4, 5, 6},
+//                {7, 8, 9}
+//        };
+//
+//        int n = matrix.length;
+//
+//        for (int i = 0; i < n; i++) {
+//            System.out.print(matrix[i][n - 1 - i] + " ");
+//        }
+
+        //Q3 Sum of diagonals
+//        int [][] matrix = { {1,2,3},
+//                {4,5,6},
+//                {7,8,9}
+//        };
+//        int sum = 0;
+//        int n= matrix.length;
+//        for (int i=0; i<n; i++) {
+//            sum += matrix[i][i];
+//        }
+//        System.out.println(sum);
+
+        //Q4 Search element in matrix
+
+
+
+
+    }
+}
