5ab6dd4d694dfb0f83ac824f61f1235b54e20b96
Rohit Korpal <rohitkorpal3@gmail.com>
2026-06-22 15:05:57 +0530

    Initial commit

--- src/BasicMaths.java
+++ src/BasicMaths.java
@@ -1,2 +1,11 @@
 public class BasicMaths {
+    static void main() {
+        //suppoose we need to get 5 from 57123 we use '%10'.
+        // '%10' get any last digit form the number
+        // '/10' to remove it after '%10' operation
+
+        int n= 53127;
+
+
+    }
 }
