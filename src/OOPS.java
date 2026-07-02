e43daccc4c249571a8f157664b25d7a4eadd619f
Rohit Korpal <rohitkorpal3@gmail.com>
2026-07-02 13:14:55 +0530

    Initial commit

--- src/OOPS.java
+++ src/OOPS.java
@@ -1,5 +1,18 @@
 public class OOPS {
     static void main() {
 
+        // OOPS developed to overcome traditional programming problems
+        // Messy codes ko organise krte hai
+        // brings modularity in code
+        // object is a real world instance like student , car , boy etc
+        // class is a template of that object
+        // oops encapsulate attributes and behavior is a single object
+        // "attributes" are state, properties of an object
+        // "behavior" is methods/functions that an object can perform
+        // object is an instance of class. ie object is created from a class
+        // every object from a same class has same properties
+        // when a object is created then only system allocates RAM to the class
+        // class is just describing common structure and behavior
+
     }
 }
