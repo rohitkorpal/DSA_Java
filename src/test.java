68669840249706c77ab711cbb474f0be569b73f8
Rohit Korpal <rohitkorpal3@gmail.com>
2026-07-03 22:50:23 +0530

    Initial commit

---
+++ src/test.java
@@ -0,0 +1,13 @@
+public class test {
+    static void main() {
+        Encapsulation A = new Encapsulation(1,12,"rahul",3,"mansi");
+        //System.out.println(A.gf);
+        System.out.println(A.name);
+        System.out.println(A.age);
+//        System.out.println(A.id);
+//        System.out.println(A.nos);
+
+        A.bunk();
+        //A.gfchatting();
+    }
+}
