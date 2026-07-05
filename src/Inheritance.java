81cbaa6d4b7b6d6812a185676de6dee3750217cd
Rohit Korpal <rohitkorpal3@gmail.com>
2026-07-05 21:10:24 +0530

    Initial commit

--- src/Inheritance.java
+++ src/Inheritance.java
@@ -2,6 +2,13 @@ public class Inheritance {
     static void main() {
         //inheritance  is "IS-A" relationship
         //for eg mammal is a animal
+        // makes code reusable and extensible
+        // public, private(can be accessed within class only) and protected(can be inherited but not accessible in main fnc, can be accessed only within class and derived class)
+        // single inheritance
+        // multilevel
+        // hierarchial - single parent multiple child class
+        // multiple - a child class made from 2 parent,does not exist in java
+        // hybrid - hierarchial + multiple
 
     }
 }
