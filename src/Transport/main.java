f625822ee85abcf04426f681372d95d958b17411
Rohit Korpal <rohitkorpal3@gmail.com>
2026-07-07 19:53:29 +0530

    Initial commit

---
+++ src/Transport/main.java
@@ -0,0 +1,15 @@
+package Transport;
+
+public class main {
+    static void main() {
+//        Car c = new Car(4,"automatic", 4,"Maruti","800");
+//        c.StartEngine();
+//        c.startAc();
+//        c.StopEngine();
+
+        MotorCycle m = new MotorCycle("splendor", "Xline", 2,"monochromic","low profile");
+        m.StartEngine();
+        m.wheelie();
+        m.StopEngine();
+    }
+}
