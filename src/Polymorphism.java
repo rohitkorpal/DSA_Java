1b3aca7b2092b06acc7661a2577c19432eed0cbc
Rohit Korpal <rohitkorpal3@gmail.com>
2026-07-08 22:51:42 +0530

    Initial commit

---
+++ src/Polymorphism.java
@@ -0,0 +1,8 @@
+public class Polymorphism {
+    static void main() {
+        // has many forms
+        // abiltiy of a single function or operator to work in diff ways based on object it is acting upon
+        // helps an object to take multiple forms
+        //Types - compile time polymorphism, runtime poly
+    }
+}
