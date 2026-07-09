5eaa39bf95c3705388ae212fa0909c552b7db0ac
Rohit Korpal <rohitkorpal3@gmail.com>
2026-07-09 11:44:07 +0530

    Add comments to explain abstraction concepts in main class

--- src/Abstraction/main.java
+++ src/Abstraction/main.java
@@ -1,4 +1,15 @@
 package Abstraction;
 
 public class main {
+    static void main() {
+
+        //Abstraction is the process of hiding the implementation details and showing only functionality to the user.
+        //In Java, abstraction is achieved using abstract classes and interfaces.
+
+        //Abstract class: An abstract class is a class that cannot be instantiated and is declared using the abstract keyword.
+        //It can have abstract methods (methods without a body) as well as concrete methods (methods with a body).
+        //Interface: An interface is a reference type in Java that is similar to a class and is a collection of abstract methods.
+        //A class implements an interface, thereby inheriting the abstract methods of the interface.
+
+    }
 }
