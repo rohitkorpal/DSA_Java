9f97cc43d1fef97e748ae5d1dcdae207bfc61dc2
Rohit Korpal <rohitkorpal3@gmail.com>
2026-07-09 12:03:36 +0530

    Implement abstraction with abstract class and child class in main.java; add .gitignore for IDE files

--- src/Abstraction/main.java
+++ src/Abstraction/main.java
@@ -1,7 +1,27 @@
 package Abstraction;
 
+// Abstract class
+abstract class Car {
+
+    // Abstract method (implementation nahi di)
+    abstract void startEngine();
+}
+
+// Child class implementation provide karegi
+class BMW extends Car {
+
+    @Override
+    void startEngine() {
+
+        // Internal implementation
+        System.out.println("Checking Battery...");
+        System.out.println("Injecting Fuel...");
+        System.out.println("Starting Engine...");
+    }
+}
+
 public class main {
-    static void main() {
+    public static void main(String[] args) {
 
         //Abstraction is the process of hiding the implementation details and showing only functionality to the user.
         //In Java, abstraction is achieved using abstract classes and interfaces.
@@ -11,5 +31,15 @@ public class main {
         //Interface: An interface is a reference type in Java that is similar to a class and is a collection of abstract methods.
         //--A class implements an interface, thereby inheriting the abstract methods of the interface.
 
+        BMW car = new BMW();
+
+        // User sirf method call karta hai
+        car.startEngine();
+
+        // User ko andar kya process ho raha hai
+        // uski details pata hone ki zarurat nahi.
+
     }
+
+
 }
