94866107cd611e2319f6f962d2f0a7ecb444c820
Rohit Korpal <rohitkorpal3@gmail.com>
2026-07-14 15:27:07 +0530

    commited changes

--- src/TimeComplexity.java
+++ src/TimeComplexity.java
@@ -5,6 +5,9 @@ public class TimeComplexity {
     //space complexity is measured in terms of big O notation
     //time complexity is measured in terms of number of operations performed by our program
 
+    // time and space complexity order
+    // O(1) < O(log n) < O ( Sqrt n) < O (n) < O (nlogn) < O (n^2) < O (n^3) < O (2^n) < O (n!) < O (n^n)
+
     // Big O --> worst case / upper bound --> no. of comparison in worst case is 'n' comparison --> O(n)
     // Theta Θ --> average case --> no. of comparison in average case is 'n/2' comparison --> Θ(n)
     // Omega Ω --> best case / lower bound --> no. of comparison in best case is '1' comparison --> Ω(1)
