6c246c5b97ddf096542c57ad3fd350b365718dab
Rohit Korpal <rohitkorpal3@gmail.com>
2026-07-14 09:46:40 +0530

    commited changes

--- src/TimeComplexity.java
+++ src/TimeComplexity.java
@@ -1,5 +1,8 @@
 public class TimeComplexity {
     //time complexity - it is just an entity by which we measure time taken by our program depending on your input size
-
+    //space complexity - it is just an entity by which we measure space taken by our program
+    //time complexity is measured in terms of big O notation
+    //space complexity is measured in terms of big O notation
+    //time complexity is measured in terms of number of operations performed by our program
 
 }
