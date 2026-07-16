60aabfd6cd05277a955542cdf30e4cad8f7e0338
Rohit Korpal <rohitkorpal3@gmail.com>
2026-07-16 23:31:54 +0530

    commited changes

--- src/TimeComplexity.java
+++ src/TimeComplexity.java
@@ -11,6 +11,11 @@ public class TimeComplexity {
 
     // Space Complexity measures: How much extra memory your program uses
 
+    // O(1) --> Constant [meaning]
+    // O(n) --> Linear [meaning]
+    // O(n^2) --> Quadratic [meaning]
+    // O(log n) --> Logarithmic [meaning]
+
     // time and space complexity order
     // [Smallest] O(1) < O (logn) < O ( Sqrt n) < O (n) < O (nlogn) < O (n^2) < O (n^3) < O (2^n) < O (n!) < O (n^n) [Biggest]
 
@@ -22,6 +27,45 @@ public class TimeComplexity {
     // O (2n) --> O (N)
 
     //space complexity of variables is constant chahe kitna bhi banao ie O(1)
+
+    // O(1) - constant time
+    // No matter input size: op remains constant. always same work ie --> O(1)
+//    int x= 10;
+//    System.out.println(x);
+
+    //O(n) - linear time
+    // work grows linearly with input size
+    // loop runs n times.
+    //for (int i = 0; i < n; i++) {
+    //    System.out.println(i);
+    // }
+
+    //O(n^2) - quadratic time
+    // work grows quadratically with input size
+    // nested loop runs n*n= n^2 times.
+    //for (int i = 0; i < n; i++) {
+    //    for (int j = 0; j < n; j++) {
+    //        System.out.println(i + " " + j);
+    //    }
+    //}
+
+    //O(logn) - logarithmic time
+    // work grows logarithmically with input size
+    // loop runs logn times.
+    // NUMBER OF STEPS = LOGN
+    //for (int i = 1; i < n; i = i * 2) {
+    //    System.out.println(i);
+    //}
+
+    //How to Calculate Time Complexity
+    // 1. ignore constants
+    // for (int i = 0; i < 2*n; i++) {
+    //    System.out.println(i);
+    //}
+    // -> O(2n) = O(n)
+
+
+
 
     //Qyestions
     //1. O(n)
