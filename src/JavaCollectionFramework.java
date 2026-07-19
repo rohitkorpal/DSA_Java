1242f6e3a62ad8bd8d83621e6f8304a360d9dfeb
rohit <rohitkorpal3@gmail.com>
2026-07-20 00:19:03 +0530

    initial commit

--- src/JavaCollectionFramework.java
+++ src/JavaCollectionFramework.java
@@ -1,5 +1,8 @@
 public class JavaCollectionFramework {
     // JCF is a set of interfaces and classes to implement various data structures and algos
     //collection is the root interface
+    static void main() {
+        System.out.println("hi hi");
+    }
 
 }
