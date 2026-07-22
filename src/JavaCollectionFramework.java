58b4f358fbc589d77f339ff0ade53fc91c3411b1
rohit <rohitkorpal3@gmail.com>
2026-07-22 16:54:17 +0530

    initial commit

--- src/JavaCollectionFramework.java
+++ src/JavaCollectionFramework.java
@@ -1,10 +1,15 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 public class JavaCollectionFramework {
     // JCF is a set of interfaces and classes to implement various data structures and algos
     //collection is the root interface
     static void main() {
         // list or collection -> interface -> object creation not possible
-
-
+        ArrayList<Integer> arr = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        Collection<Integer> collection = new ArrayList<>();
 
     }
 }
