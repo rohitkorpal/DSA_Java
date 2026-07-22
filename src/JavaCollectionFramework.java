28736d7d5d9d4baf4305df659a274258dd0aa767
rohit <rohitkorpal3@gmail.com>
2026-07-22 16:58:02 +0530

    initial commit

--- src/JavaCollectionFramework.java
+++ src/JavaCollectionFramework.java
@@ -7,9 +7,19 @@ public class JavaCollectionFramework {
     //collection is the root interface
     static void main() {
         // list or collection -> interface -> object creation not possible
-        ArrayList<Integer> arr = new ArrayList<>();
-        List<Integer> list = new ArrayList<>();
-        Collection<Integer> collection = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+
+        //add
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+        System.out.println(list);
+        list.add(60);
+        System.out.println(list);
+//        List<Integer> list = new ArrayList<>();
+//        Collection<Integer> collection = new ArrayList<>();
 
     }
 }
