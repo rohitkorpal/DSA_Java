db51b383607803343064eff66297f63201485e22
Rohit Korpal <rohitkorpal3@gmail.com>
2026-07-25 17:58:47 +0530

    commited changes

--- src/JavaCollectionFramework.java
+++ src/JavaCollectionFramework.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class JavaCollectionFramework {
     // JCF is a set of interfaces and classes to implement various data structures and algos
@@ -11,47 +8,54 @@ public class JavaCollectionFramework {
         ArrayList<Integer> list = new ArrayList<>();
 
         //add
-        list.add(10);
-        list.add(20);
-        list.add(30);
-        list.add(40);
-        list.add(50);
-        System.out.println(list);
-        list.add(60);
-        System.out.println(list);
+//        list.add(10);
+//        list.add(20);
+//        list.add(30);
+//        list.add(40);
+//        list.add(50);
+//        System.out.println(list);
+//        list.add(60);
+//        System.out.println(list);
+//
+//        //remove
+//        list.remove(0);
+//        System.out.println(list);
+//
+//        //addAll method
+//        List<Integer> list2 = new ArrayList<>();
+//        list2.add(101);
+//        list2.add(102);
+//        list2.add(103);
+//
+//        list.addAll(list2);
+//        System.out.println(list);
+//
+//        //removeAll
+//        list.removeAll(list2);
+//        System.out.println(list);
+//
+//        //size
+//        System.out.println(list.size());
+//
+//        System.out.println("printing list 2:"+list2);
+//        list2.clear();
+//        System.out.println(list2.size());
+//
+//        //iterator -> standard way to traverse over any data structure
+//
+//        //i want to traverse list using iterator
+//        Iterator<Integer> iterator = list.iterator();
+//        while(iterator.hasNext()){
+//            System.out.println("element: "+ iterator.next());
+//        }
+//        // iterator has [has next()] to iterate over the array or list
 
-        //remove
-        list.remove(0);
-        System.out.println(list);
-
-        //addAll method
-        List<Integer> list2 = new ArrayList<>();
-        list2.add(101);
-        list2.add(102);
-        list2.add(103);
-
-        list.addAll(list2);
-        System.out.println(list);
-
-        //removeAll
-        list.removeAll(list2);
-        System.out.println(list);
-
-        //size
-        System.out.println(list.size());
-
-        System.out.println("printing list 2:"+list2);
-        list2.clear();
-        System.out.println(list2.size());
-
-        //iterator -> standard way to traverse over any data structure
-
-        //i want to traverse list using iterator
-        Iterator<Integer> iterator = list.iterator();
-        while(iterator.hasNext()){
-            System.out.println("element: "+ iterator.next());
-        }
-        // iterator has [has next()] to iterate over the array or list
+        List<Integer> list3 = new ArrayList<>();
+        list3.add(10);
+        list3.add(20);
+        list3.add(30);
+        list3.add(40);
+        System.out.println(list3.get(0));
 
         
 //        List<Integer> list = new ArrayList<>();
