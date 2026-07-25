6dcae95c08efe2b41a718d912bf52d32dc591fd3
Rohit Korpal <rohitkorpal3@gmail.com>
2026-07-25 20:32:58 +0530

    commited changes

--- src/JavaCollectionFramework.java
+++ src/JavaCollectionFramework.java
@@ -4,67 +4,77 @@ public class JavaCollectionFramework {
     // JCF is a set of interfaces and classes to implement various data structures and algos
     //collection is the root interface
    public static void main(String[] args) {
-        // list or collection -> interface -> object creation not possible
-        ArrayList<Integer> list = new ArrayList<>();
-
-        //add
-//        list.add(10);
-//        list.add(20);
-//        list.add(30);
-//        list.add(40);
-//        list.add(50);
-//        System.out.println(list);
-//        list.add(60);
-//        System.out.println(list);
+//        // list or collection -> interface -> object creation not possible
+//        ArrayList<Integer> list = new ArrayList<>();
 //
-//        //remove
-//        list.remove(0);
-//        System.out.println(list);
+//        //add
+////        list.add(10);
+////        list.add(20);
+////        list.add(30);
+////        list.add(40);
+////        list.add(50);
+////        System.out.println(list);
+////        list.add(60);
+////        System.out.println(list);
+////
+////        //remove
+////        list.remove(0);
+////        System.out.println(list);
+////
+////        //addAll method
+////        List<Integer> list2 = new ArrayList<>();
+////        list2.add(101);
+////        list2.add(102);
+////        list2.add(103);
+////
+////        list.addAll(list2);
+////        System.out.println(list);
+////
+////        //removeAll
+////        list.removeAll(list2);
+////        System.out.println(list);
+////
+////        //size
+////        System.out.println(list.size());
+////
+////        System.out.println("printing list 2:"+list2);
+////        list2.clear();
+////        System.out.println(list2.size());
+////
+////        //iterator -> standard way to traverse over any data structure
+////
+////        //i want to traverse list using iterator
+////        Iterator<Integer> iterator = list.iterator();
+////        while(iterator.hasNext()){
+////            System.out.println("element: "+ iterator.next());
+////        }
+////        // iterator has [has next()] to iterate over the array or list
 //
-//        //addAll method
-//        List<Integer> list2 = new ArrayList<>();
-//        list2.add(101);
-//        list2.add(102);
-//        list2.add(103);
+//        List<Integer> list3 = new ArrayList<>();
+//        list3.add(10);
+//        list3.add(20);
+//        list3.add(30);
+//        list3.add(40);
+//        //System.out.println(list3.get(0));
+//        System.out.println(list3);
 //
-//        list.addAll(list2);
-//        System.out.println(list);
+//        list3.set(0, 100);
+//        System.out.println(list3);
 //
-//        //removeAll
-//        list.removeAll(list2);
-//        System.out.println(list);
+//        //toArray - convert a list into array
+//       Object[] array = list3.toArray();
+//       for(Object o : array){
+//           System.out.println(o);
+//       }
 //
-//        //size
-//        System.out.println(list.size());
-//
-//        System.out.println("printing list 2:"+list2);
-//        list2.clear();
-//        System.out.println(list2.size());
-//
-//        //iterator -> standard way to traverse over any data structure
-//
-//        //i want to traverse list using iterator
-//        Iterator<Integer> iterator = list.iterator();
-//        while(iterator.hasNext()){
-//            System.out.println("element: "+ iterator.next());
-//        }
-//        // iterator has [has next()] to iterate over the array or list
-
-        List<Integer> list3 = new ArrayList<>();
-        list3.add(10);
-        list3.add(20);
-        list3.add(30);
-        list3.add(40);
-        //System.out.println(list3.get(0));
-        System.out.println(list3);
-
-        list3.set(0, 100);
-        System.out.println(list3);
-
-        //toArray - convert a list into array
+//       //contains -- checks whether a list has the given element or not. returns true or false
+//       System.out.println(list3.contains(100));
 
 //        List<Integer> list = new ArrayList<>();
-//        Collection<Integer> collection = new ArrayList<>();
+
+       Collection<Integer> collection = new ArrayList<>();
+       collection.add(1);
+       System.out.println(collection);
 
     }
 }
