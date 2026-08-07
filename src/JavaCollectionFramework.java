b755df48d201585efd7f458e3cb6bdd9b5ee100a
Rohit Korpal <rohitkorpal3@gmail.com>
2026-08-07 12:42:34 +0530

    commited changes

--- src/JavaCollectionFramework.java
+++ src/JavaCollectionFramework.java
@@ -9,47 +9,47 @@ public class JavaCollectionFramework {
 //        ArrayList<Integer> list = new ArrayList<>();
 //
 //        //add
-////        list.add(10);
-////        list.add(20);
-////        list.add(30);
-////        list.add(40);
-////        list.add(50);
-////        System.out.println(list);
-////        list.add(60);
-////        System.out.println(list);
-////
-////        //remove
-////        list.remove(0);
-////        System.out.println(list);
-////
-////        //addAll method
-////        List<Integer> list2 = new ArrayList<>();
-////        list2.add(101);
-////        list2.add(102);
-////        list2.add(103);
-////
-////        list.addAll(list2);
-////        System.out.println(list);
-////
-////        //removeAll
-////        list.removeAll(list2);
-////        System.out.println(list);
-////
-////        //size
-////        System.out.println(list.size());
-////
-////        System.out.println("printing list 2:"+list2);
-////        list2.clear();
-////        System.out.println(list2.size());
-////
-////        //iterator -> standard way to traverse over any data structure
-////
-////        //i want to traverse list using iterator
-////        Iterator<Integer> iterator = list.iterator();
-////        while(iterator.hasNext()){
-////            System.out.println("element: "+ iterator.next());
-////        }
-////        // iterator has [has next()] to iterate over the array or list
+//        list.add(10);
+//        list.add(20);
+//        list.add(30);
+//        list.add(40);
+//        list.add(50);
+//        System.out.println(list);
+//        list.add(60);
+//        System.out.println(list);
+//
+//        //remove
+//        list.remove(0);
+//        System.out.println(list);
+//
+//        //addAll method
+//        List<Integer> list2 = new ArrayList<>();
+//        list2.add(101);
+//        list2.add(102);
+//        list2.add(103);
+//
+//        list.addAll(list2);
+//        System.out.println(list);
+//
+//        //removeAll
+//        list.removeAll(list2);
+//        System.out.println(list);
+//
+//        //size
+//        System.out.println(list.size());
+//
+//        System.out.println("printing list 2:"+list2);
+//        list2.clear();
+//        System.out.println(list2.size());
+//
+//        //iterator -> standard way to traverse over any data structure
+//
+//        //i want to traverse list using iterator
+//        Iterator<Integer> iterator = list.iterator();
+//        while(iterator.hasNext()){
+//            System.out.println("element: "+ iterator.next());
+//        }
+//        // iterator has [has next()] to iterate over the array or list
 //
 //        List<Integer> list3 = new ArrayList<>();
 //        list3.add(10);
@@ -73,13 +73,13 @@ public class JavaCollectionFramework {
 
 //        List<Integer> list = new ArrayList<>();
 
-        List<Integer> collection = new ArrayList<>();
-        collection.add(10);
-        collection.add(6);
-        collection.add(36);
-        collection.add(32);
-        collection.add(48);
-        System.out.println(collection);
+//        List<Integer> collection = new ArrayList<>();
+//        collection.add(10);
+//        collection.add(6);
+//        collection.add(36);
+//        collection.add(32);
+//        collection.add(48);
+        //System.out.println(collection);
 
         //sort an array list or collection
 
@@ -92,9 +92,11 @@ public class JavaCollectionFramework {
 //        System.out.println(collection);
 
         //.clone
-        ArrayList<Integer> clonedlist = (ArrayList<Integer>) ((ArrayList<Integer>) collection).clone();
-        System.out.println(" cloned list:"+clonedlist);
+//        ArrayList<Integer> clonedlist = (ArrayList<Integer>) ((ArrayList<Integer>) collection).clone();
+//        System.out.println(" cloned list:"+clonedlist);
 
+        ArrayList<Integer> anotherClonedList = new ArrayList<>();
+        anotherClonedList.ensureCapacity(550);
 
 
     }
