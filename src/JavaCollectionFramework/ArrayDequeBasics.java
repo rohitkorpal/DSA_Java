5c99c643a8a71de5bb16a8d79d5fd7c4121a01a3
Rohit Korpal <rohitkorpal3@gmail.com>
2026-08-15 14:55:03 +0530

    commited changes

---
+++ src/JavaCollectionFramework/ArrayDequeBasics.java
@@ -0,0 +1,19 @@
+package JavaCollectionFramework;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Queue;
+
+public class ArrayDequeBasics {
+    static void main() {
+
+        // ArrayDeque is a resizable array implementation of the Deque interface. It is a linear data structure that allows the insertion and removal of elements from both ends. It is not thread-safe and does not support concurrent access by multiple threads.
+        // ArrayDeque is faster than LinkedList and Stack because it uses a dynamic array to store the elements. It has a better cache locality and less memory overhead.
+        // ArrayDeque can be used as a stack (LIFO) or a queue (FIFO) depending on the methods used to add and remove elements.
+        // ArrayDeque does not allow null elements. It throws NullPointerException if you try to add null.
+        // ArrayDeque has no capacity restrictions. It grows as needed to accommodate new elements.
+        // ArrayDeque is not synchronized. If multiple threads access an ArrayDeque concurrently, and at least one of the threads modifies the deque structurally, it must be synchronized externally.
+        Deque<Integer> arrayDeque = new ArrayDeque<>();
+
+    }
+}
--- src/JavaCollectionFramework/JCF2.java
+++ src/JavaCollectionFramework/JCF2.java
@@ -8,28 +8,30 @@ public class JCF2 {
         // Java Queue interface provides functionality of queue data structure. It is used to hold the elements about to be processed. It follows First In First Out (FIFO) order to process the elements. The element which is inserted first will be removed first.
         // addition is done at the end of the queue and deletion is done from the front of the queue.
 
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(1);
-        queue.offer(2);
-        queue.offer(3);
-        System.out.println("Queue: " + queue);
+//        Queue<Integer> queue = new LinkedList<>();
+//        queue.offer(1);
+//        queue.offer(2);
+//        queue.offer(3);
+//        System.out.println("Queue: " + queue);
         // add method is used to add elements to the queue. It throws an exception if the queue is full.
         // offer method is used to add elements to the queue. It returns false if the queue is full.
         // so we use offer method to add elements to the queue.
 
         //element() method is used to retrieve, but not remove, the head of the queue. It throws an exception if the queue is empty.
-        System.out.println("Head of the queue: " + queue.element());
+//        System.out.println("Head of the queue: " + queue.element());
         //peek() method is used to retrieve, but not remove, the head of the queue. It returns null if the queue is empty.
-        System.out.println("Head of the queue: " + queue.peek());
+//        System.out.println("Head of the queue: " + queue.peek());
         //remove() method is used to retrieve and remove the head of the queue. It throws an exception if the queue is empty.
-        System.out.println("Removed head of the queue: " + queue.remove());
+//        System.out.println("Removed head of the queue: " + queue.remove());
         //poll() method is used to retrieve and remove the head of the queue. It returns null if the queue is empty.
-        System.out.println("Removed head of the queue: " + queue.poll());
-        System.out.println("Queue after removal: " + queue);
+//        System.out.println("Removed head of the queue: " + queue.poll());
+//        System.out.println("Queue after removal: " + queue);
 
         // in queue for insertion we use offer()
         // and for deletion we use poll() method.
         // For retrieval we use peek() method.
+
+
 
     }
 }
