59fa8399f276b6f941205acd65c817f5724e9d14
Rohit Korpal <rohitkorpal3@gmail.com>
2026-08-15 15:14:32 +0530

    commited changes

--- src/JavaCollectionFramework/ArrayDequeBasics.java
+++ src/JavaCollectionFramework/ArrayDequeBasics.java
@@ -14,6 +14,28 @@ public class ArrayDequeBasics {
         // ArrayDeque has no capacity restrictions. It grows as needed to accommodate new elements.
         // ArrayDeque is not synchronized. If multiple threads access an ArrayDeque concurrently, and at least one of the threads modifies the deque structurally, it must be synchronized externally.
         Deque<Integer> arrayDeque = new ArrayDeque<>();
+        arrayDeque.offer(1);
+        arrayDeque.offer(2);
+        arrayDeque.offer(3);
+        System.out.println("ArrayDeque: " + arrayDeque);
+
+        arrayDeque.poll();
+        System.out.println("ArrayDeque after poll: " + arrayDeque);
+        arrayDeque.pollFirst();
+        System.out.println("ArrayDeque after pollFirst: " + arrayDeque);
+        arrayDeque.pollLast();
+        System.out.println("ArrayDeque after pollLast: " + arrayDeque);
+        arrayDeque.offerFirst(10);
+        System.out.println("ArrayDeque after offerFirst: " + arrayDeque);
+        arrayDeque.offerLast(20);
+        System.out.println("ArrayDeque after offerLast: " + arrayDeque);
+        System.out.println("Peek first: " + arrayDeque.peekFirst());
+        System.out.println("Peek last: " + arrayDeque.peekLast());
+        System.out.println("Size of ArrayDeque: " + arrayDeque.size());
+
+
+
+
 
     }
 }
