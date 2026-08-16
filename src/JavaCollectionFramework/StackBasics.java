a5f8eea14a3e2c2c8dbbef966d7d9bb776b6a40f
Rohit Korpal <rohitkorpal3@gmail.com>
2026-08-16 13:44:15 +0530

    commited changes

---
+++ src/JavaCollectionFramework/StackBasics.java
@@ -0,0 +1,17 @@
+package JavaCollectionFramework;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+
+
+
+public class StackBasics {
+    static void main() {
+        Deque<Integer> Stack = new ArrayDeque<>();
+        Stack.push(1);
+        Stack.push(2);
+        Stack.push(3);
+        System.out.println("Stack: " + Stack);
+    }
+}
