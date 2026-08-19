98e3dc8674e88c23376b337c70d14febd94ad7bc
Rohit Korpal <rohitkorpal3@gmail.com>
2026-08-19 16:40:33 +0530

    commited changes

--- src/JavaCollectionFramework/JavaSet.java
+++ src/JavaCollectionFramework/JavaSet.java
@@ -20,32 +20,34 @@ public class JavaSet {
         // RetainAll() method is used to retain only the elements in this set that are contained in the specified collection.
         // In other words, it removes from this set all of its elements that are not contained in the specified collection.
 
-        Set<Integer> Set1 = new HashSet<>();
-        Set<Integer> Set2 = new HashSet<>();
-
-        Set1.add(10);
-        Set1.add(20);
-        Set1.add(30);
-        Set1.add(40);
-        Set1.add(50);
-
-        Set2.add(11);
-        Set2.add(21);
-        Set2.add(31);
-        Set2.add(40);
-        Set2.add(50);
-
-        Set1.retainAll(Set2);
-        System.out.println(Set1);
-        System.out.println(Set2);
-
-        System.out.println(Set1.containsAll(Set2));
-        System.out.println(Set2.containsAll(Set1));
+//        Set<Integer> Set1 = new HashSet<>();
+//        Set<Integer> Set2 = new HashSet<>();
+//
+//        Set1.add(10);
+//        Set1.add(20);
+//        Set1.add(30);
+//        Set1.add(40);
+//        Set1.add(50);
+//
+//        Set2.add(11);
+//        Set2.add(21);
+//        Set2.add(31);
+//        Set2.add(40);
+//        Set2.add(50);
+//
+//        Set1.retainAll(Set2);
+//        System.out.println(Set1);
+//        System.out.println(Set2);
+//
+//        System.out.println(Set1.containsAll(Set2));
+//        System.out.println(Set2.containsAll(Set1));
 
         //HashSet example
 //        HashSet<Integer> set = new HashSet<>();
 //        //set does not allow duplicate elements
-//        //set doesn't maintain the order of elements
+//        //set doesn't maintain the order of elements because the elements are stored in a hash table and accessed using their hash codes.
+//        The order of elements in a HashSet is not guaranteed to be the same as the order in which they were added.
+
 //        set.add(10);
 //        set.add(20);
 //        set.add(30);
@@ -64,14 +66,18 @@ public class JavaSet {
 //        System.out.println(linkedSet);
 
         //TreeSet example
-//        TreeSet<Integer> treeSet = new TreeSet<>();
-//        treeSet.add(50);
-//        treeSet.add(40);
-//        treeSet.add(30);
-//        treeSet.add(20);
-//        treeSet.add(10);
-//        System.out.println(treeSet);
+        TreeSet<Integer> treeSet = new TreeSet<>();
+        treeSet.add(50);
+        treeSet.add(40);
+        treeSet.add(20);
+        treeSet.add(10);
+        treeSet.add(20);
+        treeSet.add(10);
+        System.out.println(treeSet);
 
 
+        //HashedSet -> unordered collection of unique elements -> O(1) time complexity for add, remove, contains operations
+        //LinkedHashSet -> ordered collection of unique elements -> O(n) time complexity for add, remove, contains operations
+        //TreeSet -> sorted collection of unique elements -> O(log n) time complexity for add, remove, contains operations
     }
 }
