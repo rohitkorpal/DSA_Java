076b905362c1fc03b545342d1f4a56c8a9871114
Rohit Korpal <rohitkorpal3@gmail.com>
2026-08-19 13:15:10 +0530

    commited changes

--- src/JavaCollectionFramework/PriorityQueueBasics.java
+++ src/JavaCollectionFramework/PriorityQueueBasics.java
@@ -5,10 +5,18 @@ import java.util.Queue;
 public class PriorityQueueBasics {
     static void main() {
         Queue<Integer> PriorityQueue = new PriorityQueue<>();
+        //default behavior -> Integers -> less value -> higher priority
         PriorityQueue.offer(5);
         PriorityQueue.offer(3);
         PriorityQueue.offer(8);
+        PriorityQueue.offer(29);
+        PriorityQueue.offer(21);
+
         System.out.println("PriorityQueue: " + PriorityQueue);
+        System.out.println(PriorityQueue.poll() + " is removed from the PriorityQueue");
+        System.out.println("PriorityQueue after removal: " + PriorityQueue);
+        System.out.println(PriorityQueue.poll() + " is removed from the PriorityQueue");
+        System.out.println("PriorityQueue after second removal: " + PriorityQueue);
 
     }
 }
